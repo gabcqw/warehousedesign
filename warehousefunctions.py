26d55783d0ad5c74410707a3c09a07cb502eb5aa
gabcqw <134643615+gabcqw@users.noreply.github.com>
2023-07-13 17:14:52 +0100

    Add files via upload

--- warehousefunctions.py
+++ warehousefunctions.py
@@ -1,34 +1,34 @@
-from ast import literal_eval
 def loading_inventory(file_name):
     try:
-        with open(file_name, 'r') as file:
-            return literal_eval(file.read())
+        with open(file_name, 'r') as file1:
+            return file1.read()
     except FileNotFoundError:
         print("This file does not exist. Please create new file. ")
         return {}
     
 def saving_inventory(file_name, inventory):
-    with open(file_name, 'w') as file:
-        file.write(str(inventory))
+    with open(file_name, 'w') as file1:
+        file1.write(str(inventory))
 
-def loading_balance(file_name): #account or balance? account value is intl/float
+def loading_balance(file_name): 
     try:
-        with open(file_name, 'r') as file:
-            return literal_eval(float(file.read()))
+        with open(file_name, 'r') as file2:
+            return file2.read()
+
     except FileNotFoundError:
         print("This file does not exist. Please create new file. ")
         return {}
 def saving_balance(file_name, balance):
-    with open(file_name, 'w') as file:
-        file.write(float(balance))
+    with open(file_name, 'w') as file2:
+        file2.write(str(balance))
 
 def loading_history(file_name):#history is a list ?how to use it? loke -.readline() ? 
     try:
-        with open(file_name, 'r') as file:
-            return literal_eval(file.read())
+        with open(file_name, 'r') as file3:
+            return [file3.read()]
     except FileNotFoundError:
         print("This file does not exist. Please create new file. ")
         return []
 def saving_history(file_name, history):
-    with open(file_name, 'w') as file:
-        file.write[(history)]
+    with open(file_name, 'w') as file3:
+        file3.write(str(history))
